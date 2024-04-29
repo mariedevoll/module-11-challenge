8087775081c8a9281a605bc3178196a60c66a96c
mariedevoll <marie.devoll@gmail.com>
2024-04-29 12:59:45 -0400

    after meeting with the tutor, my code is working

--- db/store.js
+++ db/store.js
@@ -7,11 +7,10 @@ const writeFileAsync = util.promisify(fs.writeFile);
 
 class Store  {
     read() {
-        //console.log(readFileAsync("./db.json", "utf8"))
-        return readFileAsync("./db.json", "utf8")
+        return readFileAsync("./db/db.json", "utf8")
     }
-    write(notes) {
-        return writeFileAsync("./db.json", JSON.stringify(note))
+    write(note) {
+        return writeFileAsync("./db/db.json", JSON.stringify(note, null, 4))
     }
     getNotes() {
         return this.read().then((notes) => {
@@ -20,23 +19,26 @@ class Store  {
                 parsedNotes = [].concat(JSON.parse(notes))
             } catch(error) {
                 parsedNotes = []
-            }
+            } 
             return parsedNotes;
-        })
+        }) 
     }
     addNotes(notes){
+        console.log("Incoming Data: ", notes);
         const { title, text } = notes
         if(!title || !text) {
             throw new Error("Title and Text cannot be blank")
         }
         const newNote = { title, text, id: uuidv4()}
-
+        console.log('Created: ', newNote);
         return this.getNotes().then((notes) => [...notes, newNote])
         .then((updatedNotes) => this.write(updatedNotes))
         .then(() => newNote);
     }
-    removeNotes() {
-
+    removeNotes(id) {
+        return this.getNotes()
+            .then((notes) => notes.filter((notes) => notes.id !== id))
+            .then((filteredNotes) => this.write(filteredNotes))
     }
 }
 const a = new Store().getNotes().then((notes) => console.log(notes))
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -6,15 +7,38 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 
-const store = require("./db/store");
+app.use(express.static("public"));
+
+const store = require("./db/store"); 
 //routes
-app.get("api/notes", (req, res) => {
+app.get("/api/notes", (req, res) => {
     store.getNotes().then((notes) => {
         return res.status(200).json(notes)
     }).catch((error) => res.status(500).json(error))
 })
+app.post("/api/notes", (req, res) => {
+    console.log("recieved POST request to /api/notes");
+    console.log("request body:", req.body);
 
+    store.addNotes(req.body).then((notes) => {
+            //console.log("Notes added:", notes);
+        return res.status(200).json(notes)
+    }).catch((error) => res.status(500).json(error))
+})
+app.delete("/api/notes/:id", (req, res) => {
+    store.removeNotes(req.params.id).then(() => {
+        return res.status(200).json({ delete : true, id: req.params.id })
+    }).catch((error) => res.status(500).json(error))
+})
 //optimize
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/notes.html"))
+})
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/index.html"))
+})
+
+
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`)
 });
